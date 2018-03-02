e1d86d9e526d2965e7a72c8de7e465b508ef1319
Hélder Quintela <haquintela@outlook.com>
2018-03-02 16:06:13 +0000

    fev2018

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,7 +13,8 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
 
-        private Aluno aluno1;
+        private Aluno aluno1, aluno2;
+        
 
         public Form1()
         {
@@ -21,8 +22,11 @@ namespace WindowsFormsApp1
             
     }
 
+        private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Carregou no botão");
+            aluno1 = new Aluno();
+            aluno1.Nome = "José";
+            MessageBox.Show(aluno1.Nome);
                   
         }
     }
