9158daa6d1ab9650dc9ae252f00dc2b2a5bb08d2
Hélder Quintela <haquintela@outlook.com>
2018-03-02 16:05:27 +0000

    fev02

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,9 +21,7 @@ namespace WindowsFormsApp1
             
     }
 
-        private void button1_Click(object sender, EventArgs e)
         {
-            
             MessageBox.Show("Carregou no botão");
                   
         }
